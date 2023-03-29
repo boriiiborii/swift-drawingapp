008cdd0c0d810ec9ca7f8d6a7b51facb24a5cb62
Haena <ghkwlsspdl@naver.com>
2023-03-29 16:27:14 +0900

    코드리뷰 반영 및 factory클래스 구현
    size, locate, color 타입선언
    rectangle 클래스에 초기화선언부분 가변적 요소 선언
    ViewController에 makeRectangle 메서드 작성

--- DrawingApp/DrawingApp/Rectangle.swift
+++ DrawingApp/DrawingApp/Rectangle.swift
@@ -1,35 +1,51 @@
 import Foundation
 
-class Rectangle{
-    let chars = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*").map({String($0)})
-    var id : String{
-        let randomChars = (0..<9).map { _ in chars.randomElement()! }
-        let tempId = randomChars[0...2].joined() + "-" + randomChars[3...5].joined() + "-" + randomChars[6...8].joined()
-        return tempId
-    }
-    var width = Double(150)
-    var height = Double(120)
+class Rectangle : Size, Location, Color{
+    var name : String
+    var id : String
+    var width : Int
+    var height : Int
     var locatedX : Double
     var locatedY : Double
     
-    var colorR = (0...255).randomElement()
-    var colorG = (0...255).randomElement()
-    var colorB = (0...255).randomElement()
-    var alpha = (1...10).randomElement()
+    var colorR : Int
+    var colorG : Int
+    var colorB : Int
+    var alpha : Int
     
-    init(locatedX : Double, locatedY : Double){
+    init(name: String, id: String, width: Int, height: Int, locatedX: Double, locatedY: Double, colorR: Int, colorG: Int, colorB: Int, alpha: Int) {
+        self.name = name
+        self.id = id
+        self.width = width
+        self.height = height
         self.locatedX = locatedX
         self.locatedY = locatedY
+        self.colorR = colorR
+        self.colorG = colorG
+        self.colorB = colorB
+        self.alpha = alpha
     }
 }
 
 extension Rectangle : CustomStringConvertible{
     var description: String {
-        if colorR != nil && colorG != nil && colorB != nil{
-            return "(\(id), X:\(locatedX), Y:\(locatedY), W\(width), H\(height), R:\(colorR!), G:\(colorG!), B:\(colorB!), Alpha:\(alpha!)"
-        }
-           return "check your color"
-        }
+        
+        return "(name: \(name), id: \(id), X:\(locatedX), Y:\(locatedY), W\(width), H\(height), R:\(colorR), G:\(colorG), B:\(colorB), Alpha:\(alpha))"
+    }
 }
 
+protocol Size{
+    var width : Int {get}
+    var height : Int {get}
+}
 
+protocol Location{
+    var locatedX : Double {get}
+    var locatedY : Double {get}
+}
+
+protocol Color{
+    var colorR : Int {get}
+    var colorG : Int {get}
+    var colorB : Int {get}
+}
--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -2,17 +2,32 @@ import UIKit
 import OSLog
 
 class ViewController: UIViewController {
-
+    var count = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for _ in 0..<4{
+           makeRectangle()
+        }
     }
     
     @IBAction func makeRectangleButtonTapped(_ sender: Any) {
+        makeRectangle()
+    }
+    
+    
+    
+    func makeRectangle(){
+        let name = "rectangle\(count)"
+        let rectangleHeight = 120
+        let rectangleWidth = 150
         
-        let rectangle = Rectangle(locatedX: Double.random(in: 0...UIScreen.main.bounds.width), locatedY:  Double.random(in: 0...UIScreen.main.bounds.height))
-        os_log("\(rectangle.description)")
+        let newRectangle = RectangleFactory().makeRectangle(name : name, width: rectangleWidth, height: rectangleHeight, locatedX: Double.random(in: (0...UIScreen.main.bounds.width)), locatedY: Double.random(in: (0...UIScreen.main.bounds.height)))
         
+        os_log("\(newRectangle.description)")
+        count += 1
     }
     
 }
